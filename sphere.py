d3ab405d31072f90b5c76e22924885187f505b24
ziansin <pilchza@ssfs.org>
2022-09-21 08:27:48 -0400

    sphere

--- sphere.py
+++ sphere.py
@@ -1,2 +1,6 @@
 # Follow the instructions on the Variables and Expressions sheet to calculate the volume of a sphere with
 # a given radius. As a hint, when doing something to the 3rd power, Python has a special operator. r^3 will NOT work.
+r = 5
+pi = 3.14
+volume = (4/3 * pi * r ** 3)
+print("the volume of a sphere with radius", r, "is", volume)
