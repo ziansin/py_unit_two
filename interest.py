5cebfe74a722c8433660cea96db20595d9c74545
ziansin <pilchza@ssfs.org>
2022-09-21 08:31:17 -0400

    interest

--- interest.py
+++ interest.py
@@ -1,2 +1,8 @@
 # Use this file to calculate the amount of compound interest. Follow the instructions on the
 # Variables and Expressions sheet carefully.
+P = 10000
+n = 12
+r = 0.08
+t = 10
+A = (P * (1 + r/n) ** (n * t))
+print("The amount of compound interest is", A)
