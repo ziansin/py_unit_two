8f984d0a6c5c105b7e5bed94cce90ffd9c604aa7
ziansin <pilchza@ssfs.org>
2022-09-21 08:23:19 -0400

    triangle

--- triangle.py
+++ triangle.py
@@ -2,7 +2,7 @@ base = 10
 height = 5
 
 # Please fix the expression below with the correct formula for the area of a triangle.
-area =
+area = (base * height)/2
 
 # This statement should print 25 when the program is run
 print("The area of the triangle is", area)
